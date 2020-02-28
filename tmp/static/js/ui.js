67b04f153d4d86bdb890ad85829ad674a1528129
junfrenkixp <penin13579@gmail.com>
2020-02-28 03:39:12 +0300

    test

--- tmp/static/js/ui.js
+++ tmp/static/js/ui.js
@@ -1,17 +1,21 @@
-;
+;(function() {
+  window.addEventListener('DOMContentLoaded', () => {
+    window.svg4everybody()
 
-(function () {
-  window.addEventListener('DOMContentLoaded', function () {
-    window.svg4everybody();
-    $('.js-dropdown-box').each(function () {
+    $('.js-dropdown-box').each(function() {
       $(this).dropdown({
-        prefix: $(this).data('prefix')
-      });
-    });
-  });
-  document.documentElement.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-  }, false);
-})();
+        prefix: $(this).data('prefix'),
+      })
+    })
+  })
+
+  document.documentElement.addEventListener(
+    'touchstart',
+    function(event) {
+      if (event.touches.length > 1) {
+        event.preventDefault()
+      }
+    },
+    false,
+  )
+})()
